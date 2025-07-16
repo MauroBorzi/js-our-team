dff71ebb24426c727fc963192c6a8c02c75d12f3
MauroBorzi <mauroborzi91@gmail.com>
2025-07-16 17:48:51 +0200

    [add]bonusForm

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,44 +3,46 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "female3.png"
   }
 ];
 
 
-// definisco la funzione che mi genera le card
+// definisco la funzioni
 
 const addCard = (member) => {
+
+  // funzione che genera le card nel DOM
 
   const { name, role, email, img } = member
 
@@ -48,7 +50,7 @@ const addCard = (member) => {
         <div class="card mb-3 rounded-0 border-0 myBg-black">
           <div class="row g-0">
             <div class="col-4">
-              <img src="./assets/${img}" class="img-fluid" alt="...">
+              <img src="./assets/img/${img}" class="img-fluid" alt="...">
             </div>
             <div class="col-8">
               <div class="card-body">
@@ -63,18 +65,65 @@ const addCard = (member) => {
   return generalities
 }
 
+const renderCards = (teamMembers) => {
+  // ciclo gli elementi da inserire nel DOM
+
+  for (i = 0; i < teamMembers.length; i++) {
+
+    let card = addCard(teamMembers[i])
+
+    cardMember.innerHTML += card
+
+  }
+}
+
+
 // richiamo gli elementi del DOM
 
 const cardMember = document.getElementById(`team-member`)
+const addMember = document.getElementById(`add-member`)
 
 
+// creo l'evento una volta premuto il pulsante 
 
-// ciclo gli elementi da inserire nel DOM
+addMember.addEventListener(`click`, function (e) {
+  e.preventDefault()
 
-for (i = 0; i < teamMembers.length; i++) {
+  cardMember.innerHTML = ''
 
-  let card = addCard(teamMembers[i])
+  const name = document.getElementById(`name`).value
+  const role = document.getElementById(`role`).value
+  const email = document.getElementById(`email`).value
+  const img = document.getElementById(`img`).value
 
-  cardMember.innerHTML += card
+  // verifico la consistenza dei dati
 
-}
+  if (name == '' || role == '' || email == '' || img == '') {
+    alert(`Tutti i campi sono obbligatori!!!`)
+    return
+  }
+
+  // creo e aggiungo il nuovo oggetto
+
+  const newMember = {
+    name,
+    role,
+    email,
+    img
+  }
+
+  teamMembers.push(newMember)
+
+  // dopo il pusch svuoto i campi della from
+
+  document.getElementById(`name`).value = ''
+  document.getElementById(`role`).value = ''
+  document.getElementById(`email`).value = ''
+  document.getElementById(`img`).value = ''
+
+  renderCards(teamMembers)
+
+})
+
+renderCards(teamMembers)
+
